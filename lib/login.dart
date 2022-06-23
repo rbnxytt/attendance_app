56d461f9d8b3da91a93883255b98b65a36c91176
Rob <rob@iMac-ni-Bobot.local>
2022-06-23 23:36:01 +0800

    second commit

---
+++ lib/login.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:my_app/widgets/background.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LoginPage extends StatefulWidget {
+  static const String id = 'login-page';
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController? emailController, passwordController;
+
+  @override
+  void initState() {
+    emailController = TextEditingController();
+    passwordController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    emailController!.dispose();
+    passwordController!.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          const Background(),
+          Center(
+            child: Container(
+              height: 550,
+              width: 350.0,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Image.asset('assets/logo/company_logo.jpeg'),
+                  Text(
+                    'Welcome',
+                    style: GoogleFonts.montserrat(
+                        fontWeight: FontWeight.w700, fontSize: 25.0),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 30.0, vertical: 20.0),
+                    child: TextFormField(
+                      controller: emailController,
+                      decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: 'Username or Email'),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 30.0, vertical: 20.0),
+                    child: TextFormField(
+                      controller: passwordController,
+                      obscureText: true,
+                      keyboardType: TextInputType.visiblePassword,
+                      decoration: const InputDecoration(
+                          border: OutlineInputBorder(), hintText: 'Password'),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8.0),
+                            color: const Color.fromARGB(255, 231, 38, 102)),
+                        width: double.infinity,
+                        height: 50.0,
+                        child: Center(
+                          child: Text('LOGIN',
+                              style: GoogleFonts.roboto(color: Colors.white)),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Spacer(),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
